472a123d20a06d053422761c2c71bf2576f5cdc9
Akshay04205 <akshay.cse.2005@gmail.com>
2026-07-12 23:09:23 +0530

    Update 03_password_genrator.py

--- 03_password_genrator.py
+++ 03_password_genrator.py
@@ -7,12 +7,11 @@ print("=== Random Password Generator ===")
 length = int(input("Enter password length: "))
 
 # User choices
-#use_upper = input("Include Uppercase letters? (y/n): ").lower() == "y"
-#use_lower = input("Include Lowercase letters? (y/n): ").lower() == "y"
-#use_numbers = input("Include Numbers? (y/n): ").lower() == "y"
-#use_symbols = input("Include Special Characters? (y/n): ").lower() == "y"
+use_upper = input("Include Uppercase letters? (y/n): ").lower() == "y"
+use_lower = input("Include Lowercase letters? (y/n): ").lower() == "y"
+use_numbers = input("Include Numbers? (y/n): ").lower() == "y"
+use_symbols = input("Include Special Characters? (y/n): ").lower() == "y"
 
-# Character pool
 characters = ""
 
 if use_upper:
@@ -27,7 +26,6 @@ if use_numbers:
 if use_symbols:
     characters += string.punctuation
 
-# Check if at least one option is selected
 if not characters:
     print("Error: Select at least one character type.")
 else:
